05464260abf054d96f83cf29b1e30b559ddebc51
Yoorkin <Yoorkin@users.noreply.github.com>
2023-04-12 08:05:27 +0000

    deploy: raelidea/raelide@46d1d3fd0f44e118ca1657d27c34f6fd1b9f386d

--- assets/index-98ffc9da.js
+++ assets/index-98ffc9da.js
@@ -286,7 +286,7 @@ func init {
 func concat <X> (self: list<X>, ys: list<X>): list<X> {
   match self {
   | Nil => ys
-  | Cons((x, rest)) => Cons(x, concat(rest, ys))
+  | Cons(x, rest) => Cons(x, concat(rest, ys))
   }
 }
 
@@ -294,7 +294,7 @@ func concat <X> (self: list<X>, ys: list<X>): list<X> {
 func map <X, Y> (self: list<X>, f: (X) => Y): list<Y> {
   match self {
   | Nil => Nil
-  | Cons((x, rest)) => Cons(f(x), map(rest, f))
+  | Cons(x, rest) => Cons(f(x), map(rest, f))
   }
 }
 
@@ -303,7 +303,7 @@ func reverse <X> (self: list<X>): list<X> {
   fn go (acc, xs: list<X>) {
     match xs {
     | Nil => acc
-    | Cons((x, rest)) => go(Cons(x, acc), rest)
+    | Cons(x, rest) => go(Cons(x, acc), rest)
     }
   }
   go(Nil, self)
@@ -313,7 +313,7 @@ func reverse <X> (self: list<X>): list<X> {
 func iter <X> (self: list<X>, f: (X) => unit): unit {
   match self {
   | Nil => ()
-  | Cons((x, rest)) => f(x); iter(rest, f)
+  | Cons(x, rest) => f(x); iter(rest, f)
   }
 }
 
@@ -331,12 +331,12 @@ func stream<T>(self : array<T>): list<T>{
 
 // collect each elements of list into new array
 func collect<T>(self : list<T>): array<T>{
-  let Cons((x,_)) = self
+  let Cons(x,_) = self
   let ary = array_make(self.length(),x)
   fn go(xs,idx){
     match xs{
     | Nil => ()
-    | Cons((x,xs)) => ary[idx]=x; go(xs,idx+1)
+    | Cons(x,xs) => ary[idx]=x; go(xs,idx+1)
     }
   }
   ary
@@ -346,7 +346,7 @@ func collect<T>(self : list<T>): array<T>{
 func filter<T>(self : list<T>, predicate : (T)=>bool) : list<T>{
   match self{
   | Nil => Nil
-  | Cons((x,xs)) => 
+  | Cons(x,xs) => 
       if predicate(x) { 
         Cons(x, filter(xs, predicate)) 
       } else {
@@ -359,14 +359,14 @@ func filter<T>(self : list<T>, predicate : (T)=>bool) : list<T>{
 func length<T>(self : list<T>): int{
   match self{
   | Nil => 0
-  | Cons((_,xs)) => 1 + xs.length()
+  | Cons(_,xs) => 1 + xs.length()
   }
 }
 
 func reduce<T>(self : list<T>, accumulator : (T,T)=>T, initial : T) : T{
   match self{
   | Nil => initial
-  | Cons((x,xs)) => reduce(xs, accumulator, accumulator(initial,x))
+  | Cons(x,xs) => reduce(xs, accumulator, accumulator(initial,x))
   }
 }
 
@@ -375,7 +375,7 @@ func print_by<T>(self : list<T>, show : (T)=>unit): unit{
   "[ ".output()
   match self{
   | Nil => ()
-  | Cons((h,t)) => 
+  | Cons(h,t) => 
       show(h);
       t.iter(fn(x){", ".output();show(x)})
   }
